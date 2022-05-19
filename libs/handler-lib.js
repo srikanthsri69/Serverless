0cc7429dbc40ab1fa918e41ef8de4342d45b06e6
puneetha <84407@global.ul.com>
2022-05-19 11:03:19 +0530

    Added Cross-Origin Resource Sharing

--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -9,8 +9,9 @@ export default function handler(lambda) {
         body = { error: e.message };
         statusCode = 500; }
     // Return HTTP response
-return {
-    statusCode,
-    body: JSON.stringify(body), }; }; }
-
-
+    return {
+        statusCode,
+        body: JSON.stringify(body),
+        headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true, }, };}}
